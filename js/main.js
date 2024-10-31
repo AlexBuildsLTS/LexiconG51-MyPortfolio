10548024d2167421127b87dfa15d3bec003cbb81
hostghost <ALex.youssef@live.com>
2024-10-31 02:46:59 +0100

    Added more content, uploaded images, links to github,linkedin, changed backgrund color

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 function submitForm(event) {
     event.preventDefault(); // Prevent the default form submission
-    alert("Thank you for reaching out, " + document.getElementById("firstName").value + " " + document.getElementById("lastName").value + 
-          "! Your message has been sent to Alex Youssef. I will get back to you at " + document.getElementById("email").value +
-          ". You can also reach me directly at alex.youssef@live.com.");
 
-    // Optional: Reset the form fields after submission
+    // Get form values
+    const firstName = document.getElementById("firstName").value.trim();
+    const lastName = document.getElementById("lastName").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    // Display alert message
+    alert(`Thank you for reaching out, ${firstName} ${lastName}! Your message has been sent to Alex Youssef. I will get back to you at ${email}. You can also reach me directly at alex.youssef@live.com.`);
+
+    // Reset the form fields after submission
     document.getElementById("firstName").value = "";
     document.getElementById("lastName").value = "";
     document.getElementById("email").value = "";
